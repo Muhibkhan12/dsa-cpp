761db839b7da3ed383ffb2827e8ee059b716ee42
Muhibkhan12 <Muhibkhan2299@gmail.com>
2026-04-12 03:15:08 +0500

    done

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -10,11 +10,59 @@ public:
         data = val;
         next = NULL;
     }
-}
+};
+class List{
+    Node* head;
+    Node* tail;
+public:
+    List(){
+        head = tail = NULL;
+    }
 
-Node* head; 
-Node* tail;
+    Node *getHead(){
+        return head;
+    }
+
+    void pushFront(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode;
+            return;
+        }else{
+            newNode->next = head;
+            head = newNode; 
+        }
+    }
+
+    void pushBack(int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = tail = NULL;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void print(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout << temp->data << "->";
+            temp = temp->next;   
+            if(temp == NULL){
+                cout << "end";
+            }
+        }
+    }
+};
+
 int main(){
 
+    List ll;
+    ll.pushFront(1);
+    ll.pushFront(2);
+    ll.pushFront(3);
+    ll.pushBack(4);
+    ll.print();
     return 0;
-}
+};
