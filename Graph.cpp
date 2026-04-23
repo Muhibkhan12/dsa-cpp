9e10c27bb2c577bc1ace127379e552fe6832e7fc
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-04-23 21:53:53 +0500

    graphs addEdge

--- Graph.cpp
+++ Graph.cpp
@@ -10,7 +10,6 @@ class Graph{
     int vertices;
     Node** adjList;
 
-
     Graph* createGraph(int v){
         Graph* g = new Graph;
         g->vertices  = v;
@@ -20,6 +19,18 @@ class Graph{
             g->adjList[i] = NULL;
         };
         return g;
+    }
+
+    void addEdge(Graph* g, int src, int dest){
+        Node* newNode = new Node;
+        newNode->data = dest;
+        newNode->next = g->adjList[src];
+        g->adjList[src] = newNode;
+
+        Node* newNode2 = new Node;
+        newNode2->data = src;
+        newNode2->next = g->adjList[dest];
+        g->adjList[dest] = newNode2;
     }
 };
 
