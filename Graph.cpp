926d14f1cc48fdedff23c49c1038f336f18af7e3
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-04-24 21:04:11 +0500

    graphs print

--- Graph.cpp
+++ Graph.cpp
@@ -13,27 +13,25 @@ class Graph{
         int vertices;
         Node** adjList;
 
-        Graph* createGraph(int v){
-            Graph* g = new Graph;
-            g->vertices  = v;
-            g->adjList = new Node*[v];
+        Graph(int v) {
+        vertices = v;
+        adjList = new Node*[v];
 
-            for(int i=0; i<v; i++){
-                g->adjList[i] = NULL;
-            };
-            return g;
+        for (int i = 0; i < v; i++) {
+            adjList[i] = NULL;
         }
+    }
 
-        void addEdge(Graph* g, int src, int dest){
+        void addEdge( int src, int dest){
             Node* newNode = new Node;
             newNode->data = dest;
-            newNode->next = g->adjList[src];
-            g->adjList[src] = newNode;
+            newNode->next = adjList[src];
+            adjList[src] = newNode;
 
             Node* newNode2 = new Node;
             newNode2->data = src;
-            newNode2->next = g->adjList[dest];
-            g->adjList[dest] = newNode2;
+            newNode2->next = adjList[dest];
+            adjList[dest] = newNode2;
         }
 
         void printGraph(){
