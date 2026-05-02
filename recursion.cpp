9cd51e3826e873c8f54809ff6ed55d9602c6b63f
Muhibkhan12 <Muhibkhan2299@gmail.com>
2026-05-02 17:13:54 +0500

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedList{
+    Node* head;
+    Node* tail;
+public:
+    LinkedList(){
+        head = tail = NULL;
+    }
+
+    Node *getHead(){
+        return head;
+    }
+
+};
+
+
+int main(){
+
+}
