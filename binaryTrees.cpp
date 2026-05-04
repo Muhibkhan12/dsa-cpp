0c071306c9f09cd6d1312798571b0e372f266469
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-05-04 19:23:26 +0500

    Binary tree done

--- binaryTrees.cpp
+++ binaryTrees.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
 using namespace std;
 
+// int arr[11] = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+// static int idx = -1;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val){
+            data = val;
+            left = NULL;
+            right = NULL;
+        }
+
+        void inOrder(Node* root){
+            if(root == NULL){
+                cout << "Tree is empty";
+                return;
+            }else{
+                inOrder(root->left);
+                cout << root->data << " ";
+                inOrder(root->right);
+            }
+        }
+
+        void preOrder(Node* root){
+            if(root == NULL){
+                cout << "Tree is empty";
+                return;
+            }else{
+                cout << root->data << " ";
+                preOrder(root->left);
+                preOrder(root->right);
+            }
+        }
+
+        void postOrder(Node* root){
+            if(root == NULL){
+                cout << "Tree is empty" << " ";
+                return;
+            }else{
+                postOrder(root->left);
+                postOrder(root->right);
+                cout << root->data << " "; 
+            }
+        }
+};
+
 int main(){
+
 
 
     return 0;
